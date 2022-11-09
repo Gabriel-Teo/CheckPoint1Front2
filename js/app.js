93feb719321bac6fca359df5e141584cbbee97e2
Igor Matos <iw-matos@hotmail.com>
2022-11-09 20:47:13 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,43 @@
+
+var buttonAdd = document.getElementById('postar');
+
+buttonAdd.addEventListener('click', function (event) {
+    event.preventDefault()
+})
+
+buttonAdd.addEventListener('click', function addCard() {
+    let titulo = document.getElementById('titulo');
+    let img = document.getElementById('url');
+    let desc = document.getElementById('descricao');
+    let container = document.getElementById('container')
+    let item =
+        `
+    <div class="card">
+        <img src="${img.value}" class="imagem">
+        <h3>${titulo.value}</h3>
+        <p class="description">
+        ${desc.value}
+        </p>
+        <div class="botao1">
+            <button class="botao">Saiba Mais</button>
+        </div>
+    </div>
+    `;
+
+    if (titulo.value === '' || titulo.value.length < 4) {
+        buttonAdd.disabled = true;
+        return '';
+    } else {
+        container.innerHTML += item;
+    }
+});
+
+window.addEventListener('click', function () {
+    if (titulo.value === '' || titulo.value.length < 4) {
+        buttonAdd.disabled = true;
+        return '';
+    } else {
+        buttonAdd.disabled = false;
+    }
+})
+
